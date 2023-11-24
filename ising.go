d81be6d0e0ba06fa86120105ce51a5d26ccaba62
AhmAlgiz <a.ahmatzyanov@yandex.ru>
2023-11-24 21:41:40 +0300

    3

--- ising.go
+++ ising.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 type Ising struct {
-	T   int
+	T   float64
 	J   int
 	N   int
+	Kb  float64
 	Net [][]int
 }
 
@@ -22,6 +24,32 @@ func (s *Ising) M() int {
 	return m
 }
 
+func (s *Ising) P(e float64) float64 {
+	return math.Exp((-1.0) * e / (s.Kb * s.T))
+}
+
+func (s *Ising) H(i, j int) int {
+	if i == 0 {
+		if j == 0 {
+			return s.Net[i+1][j] + s.Net[i][j+1] + s.Net[s.N-1][j] + s.Net[i][s.N-1]
+		}
+		if j == s.N-1 {
+			return s.Net[i][j-1] + s.Net[i+1][j] + s.Net[s.N-1][j] + s.Net[i][s.N-1]
+		}
+		return s.Net[i][j-1] + s.Net[i][j+1] + s.Net[s.N-1][j] + s.Net[i+1][j]
+	}
+	if i == s.N {
+		if j == 0 {
+			return s.Net[i-1][j] + s.Net[i][j+1] + s.Net[0][j] + s.Net[i][s.N-1]
+		}
+		if j == s.N-1 {
+			return s.Net[i-1][j] + s.Net[i][j-1] + s.Net[0][j] + s.Net[i][0]
+		}
+		return s.Net[i][j-1] + s.Net[i][j+1] + s.Net[i-1][j] + s.Net[0][j]
+	}
+	return s.Net[i][j-1] + s.Net[i][j+1] + s.Net[i-1][j] + s.Net[i+1][j]
+}
+
 func (s *Ising) Fill() {
 	s.Net = make([][]int, 0, s.N)
 	for i := 0; i < s.N; i++ {
@@ -33,16 +61,18 @@ func (s *Ising) Fill() {
 	}
 }
 
-func (s *Ising) Switch(i, j uint32) {
+func (s *Ising) Switch() {
 
 }
 
 func main() {
 	s := Ising{
-		T: 2,
-		J: 1,
-		N: 10,
+		T:  2.0,
+		J:  1,
+		Kb: 1.0,
+		N:  10,
 	}
 	s.Fill()
-
+	fmt.Println(s.M())
+	fmt.Println(s.H(0, 5))
 }
